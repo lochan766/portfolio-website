6b7b2631124c471eb918e1bc4f240bc0da7e1f86
Lochan Samser Rana <lochan.rana766@gmail.com>
2024-11-21 23:13:40 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+
+function emailSend() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "lochan.rana766@gmail.com",
+        Password: "AC81F9E66CC53735A8584C864632A5F916A7",
+        To: 'lochanrana777@gmail.com',
+        From: "lochan.rana766@gmail.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
+
+*/
